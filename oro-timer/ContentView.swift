ce746f0098cabe1c9ff2e55a2e28b62e92d4ad94
adam-han <109483705+adam-han@users.noreply.github.com>
2025-10-13 19:40:01 -0400

    add temp button in to test timer functionality

--- oro-timer/ContentView.swift
+++ oro-timer/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                let temp_timer = TimerClass(seconds_left: 10)
+                temp_timer.startTimer()
+            } label: {
+                Text("Start Timer")
+            }
         }
-        .padding()
     }
 }
 
