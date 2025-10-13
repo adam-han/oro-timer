9a2347688648190e4f141d40ce5e5abbf05149fd
adam-han <109483705+adam-han@users.noreply.github.com>
2025-10-13 17:07:23 -0400

    initial xcode project setup

---
+++ oro-timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  oro-timer
+//
+//  Created by Adam Han on 2025-10-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ oro-timer/oro_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  oro_timerApp.swift
+//  oro-timer
+//
+//  Created by Adam Han on 2025-10-12.
+//
+
+import SwiftUI
+
+@main
+struct oro_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
